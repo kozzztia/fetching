4a9d12d014bc293a678b1909639d3af2570c9235
kozzztia <kozzztia@gmail.com>
2024-10-24 10:29:27 +0300

    finish tests with proxy

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
-const key = 'MWQsZflYAuWLfpM9rIg2xgznEHJgS73B';
-const helpUrl = "https://api.allorigins.win/get?url=";
-
-
 const fetchPosts = async () => {
-    console.log('start')
+    const body = document.querySelector('main');
     try {
-        console.log('try')
-        const response = await fetch( helpUrl + encodeURIComponent(`https://wallhaven.cc/api/v1/search?apikey=${key}&page=2`), {
-        // const response = await fetch(`https://whvn.cc/lmk1zp`,{
-          headers: {
-              'Content-Type': 'application/json',
-          }  
+        console.log('try');
+        const response = await fetch('https://proxy-server-psi-ten.vercel.app/api/proxy?page=4&category=-100&q=cat', {
+            headers: {
+                'Content-Type': 'application/json',
+            }  
         });  
+
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
-        const result = await response.json();
-        const {data,meta} = JSON.parse(result.contents);
-        console.log(data); 
-        console.log(meta); 
+
+        const {data, meta} = await response.json();
+
+        console.log('Data:', data);
+        console.log('meta:', meta);
+        const section = document.querySelector('main');
+        data.forEach(post => {
+            const img = document.createElement('img');
+            img.src = post.thumbs.original;
+            section.appendChild(img);
+        })
+
+
     } catch (error) {
-        console.log(error.message); 
+        console.log(error); 
     } finally {
         console.log("stop fetching"); 
     }
